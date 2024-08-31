9e27a523a1b703ccde1e02b77caa9e76e04db71e
Debesh Mondal <debeshprofessional05@gmail.com>
2024-08-31 09:11:03 +0530

    Update item container with lazy loading for images and dynamic price details

--- PROJECTS/MYNTRA CLONE/scripts/index.js
+++ PROJECTS/MYNTRA CLONE/scripts/index.js
@@ -1,34 +1,46 @@
 let itemsContainerElement = document.querySelector('.items-container');
 
-let item ={
-   item_image:' images/1.jpg',
+// let item ={
+//    item_image:' images/1.jpg',
 
-   rating:{
-    stars: 4.5,
-    noof_reviews: 1400,
-   },
+//    rating:{
+//     stars: 4.5,
+//     noof_reviews: 1400,
+//    },
 
-   company_name: 'Carlton London',
-   item_name: 'Rhodium-Plated CZ Floral Studs',
+//    company_name: 'Carlton London',
+//    item_name: 'Rhodium-Plated CZ Floral Studs',
 
-   curremt_price: 'Rs 649',
-   original_price: 'Rs 1049',
-   discount: '42'
+//    curremt_price: 'Rs 649',
+//    original_price: 'Rs 1049',
+//    discount: '42'
 
-}
+// }
 
-itemsContainerElement.innerHTML = `
+
+let innerHTML = ' ';
+items.forEach(item=> {
+    innerHTML+=
+    `
 <div class="item-container">
-                <img class="item-image" src="${item.item_image}" alt="item image">
+                <img class="item-image" src="${item.image}" alt="item image">
                 <div class="rating">
-                    ${item.rating.stars} ⭐ | ${item.rating.noof_reviews}
+                    ${item.rating.stars} ⭐ | ${item.rating.count}
                 </div>
-                <div class="company-name">${item.company_name}</div>
+                <div class="company-name">${item.company}</div>
                 <div class="item-name">${item.item_name}</div>
                 <div class="price">
-                    <span class="current-price">${item.curremt_price}</span>
+                    <span class="current-price">${item.current_price}</span>
                     <span class="original-price">${item.original_price}</span>
-                    <span class="discount">(${item.discount}% OFF)</span>
+                    <span class="discount">(${item.discount_percentage}% OFF)</span>
                 </div>
                 <button class="btn-add-bag">Add to Bag</button>
-            </div>`;
+            </div>`
+})
+
+
+
+
+
+
+itemsContainerElement.innerHTML = innerHTML ;
