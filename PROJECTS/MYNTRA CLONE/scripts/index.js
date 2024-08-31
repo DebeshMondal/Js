33f7708dc3e61ebcf3e6f96910fb1dde8e248736
Debesh Mondal <debeshprofessional05@gmail.com>
2024-08-31 08:31:39 +0530

    feat: Update item container with lazy loading for images and additional details

--- PROJECTS/MYNTRA CLONE/scripts/index.js
+++ PROJECTS/MYNTRA CLONE/scripts/index.js
@@ -1,16 +1,25 @@
 let itemsContainerElement = document.querySelector('.items-container');
 
 let item ={
-    
+   item_image:' images/1.jpg',
+
+   rating:{
+    stars: 4.5,
+    noof_reviews: 1400,
+   },
+
+   company_name: 'Carlton London',
+   item_name: 'Rhodium-Plated CZ Floral Studs',
 }
 
-itemsContainerElement.innerHTML = `<div class="item-container">
-                <img class="item-image" src="images/1.jpg" alt="item image">
+itemsContainerElement.innerHTML = `
+<div class="item-container">
+                <img class="item-image" src="${item.item_image}" alt="item image">
                 <div class="rating">
-                    4.5 ⭐ | 1.4k
+                    ${item.rating.stars} ⭐ | ${item.rating.noof_reviews}
                 </div>
-                <div class="company-name">Carlton London</div>
-                <div class="item-name">Rhodium-Plated CZ Floral Studs</div>
+                <div class="company-name">${item.company_name}</div>
+                <div class="item-name">${item.item_name}</div>
                 <div class="price">
                     <span class="current-price">Rs 649</span>
                     <span class="original-price">Rs 1049</span>
